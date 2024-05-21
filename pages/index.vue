fae236f7c97ff149c365cbbc6dcb02076881a9ca
B. Erfransyah Levi Darmawan <franslevi008@gmail.com>
2024-05-21 20:50:24 +0700

    feat: update main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,64 @@
 <template>
-  <main class="w-full flex flex-col prose max-w-none items-center px-10 py-20 bg-slate-50 min-h-screen">
-    <h1 class="p-3 m-0">Weekend Ngoding</h1>
-    <span>Learn to build website via source code (<a href="https://github.com/erfrandarmawan/web-weekend-ngoding" target="_blank" rel="noreferrer noopener">github.com/erfrandarmawan/web-weekend-ngoding)</a></span>
+  <main class="w-full flex flex-col items-center px-10 py-20 bg-slate-50 min-h-screen">
+    <article class="prose flex flex-col max-w-none items-center">
+      <h1 class="p-3 m-0">Weekend Ngoding</h1>
+      <span>Learn to build website via source code (<a href="https://github.com/erfrandarmawan/web-weekend-ngoding" target="_blank" rel="noreferrer noopener">github.com/erfrandarmawan/web-weekend-ngoding)</a></span>
+    </article>
 
     <div class="w-8/12 grid grid-cols-2 gap-5 mt-10">
-      <a class="card bg-base-100 shadow-xl no-underline" href="/theme/senyawa">
-        <div class="card-body">
-          <span>#1 Senyawa Plus</span>
+      <a class="card bg-base-100 shadow-xl no-underline gap-0 group" href="/theme/senyawa">
+        <figure class="bg-slate-200">
+          <img src="https://ik.imagekit.io/b3amk7ihm/1716298815580.webp" alt="Senyawa Plus" class="object-cover group-hover:scale-110 duration-300" />
+        </figure>
+        <div class="card-body p-5">
+          <span class="card-title group-hover:text-primary duration-300">#1 Senyawa Plus</span>
         </div>
       </a>
 
-      <a class="card bg-base-100 shadow-xl no-underline" href="/theme/tjendana">
-        <div class="card-body">
-          <span>#2 Tjendana Bistro</span>
+      <a class="card bg-base-100 shadow-xl no-underline gap-0 group" href="/theme/tjendana">
+        <figure class="bg-slate-200">
+          <img src="https://ik.imagekit.io/b3amk7ihm/1716299244692.webp" alt="Tjendana Bistro" class="object-cover group-hover:scale-110 duration-300" />
+        </figure>
+        <div class="card-body p-5">
+          <span class="card-title group-hover:text-primary duration-300">#2 Tjendana Bistro</span>
         </div>
       </a>
     </div>
   </main>
 </template>
+
+<script setup lang="ts">
+  // Import library
+  import gsap from 'gsap';
+  import { TextPlugin } from 'gsap/TextPlugin';
+  import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+  // Heading and set body attributes
+  useHead({
+    title: 'Weekend Ngoding',
+    viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    charset: 'utf-8',
+    meta: [
+      { name: 'description', content: 'Learn to build website via source code' },
+      { name: 'keywords', content: 'weekend ngoding, ngoding, informatika, coding, code, front end, frontend, learning, ui, web, html, css, nuxt' },
+      { name: 'author', content: 'Erfran Darmawan' },
+
+      { name: 'og:title', content: 'Weekend Ngoding' },
+      { name: 'og:site_name', content: 'Weekend Ngoding' },
+      { name: 'og:description', content: 'Learn to build website via source code' },
+      { name: 'og:url', content: 'https://weekend-ngoding.develobe.id/' },
+      { name: 'og:image', content: 'TODO' },
+      { name: 'og:image:secure_url', content: 'TODO' },
+      { name: 'og:image:type', content: 'image/png' },
+      { name: 'og:image:width', content: '300' },
+      { name: 'og:image:height', content: '213' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:locale', content: 'en_US' },
+
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: 'Weekend Ngoding' },
+      { name: 'twitter:description', content: 'Learn to build website via source code' },
+      { name: 'twitter:image', content: 'TODO' },
+    ],
+  })
+</script>
