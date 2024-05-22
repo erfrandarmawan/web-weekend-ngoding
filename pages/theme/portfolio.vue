75bf791815e89a803b3d4e9f07f087463d0fd7f1
B. Erfransyah Levi Darmawan <franslevi008@gmail.com>
2024-05-22 21:06:39 +0700

    feat: add responsive design for portfolio theme

--- pages/theme/portfolio.vue
+++ pages/theme/portfolio.vue
@@ -2,35 +2,35 @@
   <main class="w-full h-auto flex flex-col bg-slate-100">
     <!-- Main Section -->
     <section class="w-full h-screen flex flex-col items-center justify-center">
-      <div id="main-image" class="w-[300px] h-[300px] rounded-full group overflow-hidden drop-shadow-xl ring-8 ring-white"
+      <div id="main-image" class="w-[200px] h-[200px] md:w-[250px] md:h-[250px] xl:w-[300px] xl:h-[300px] rounded-full group overflow-hidden drop-shadow-xl ring-8 ring-white"
         :class="isMounted ? 'opacity-100' : 'opacity-0'">
         <img src="https://ik.imagekit.io/b3amk7ihm/1716347873206.webp" class="w-full h-full object-cover group-hover:scale-110 duration-300 object-top" />
       </div>
 
-      <h1 id="main-text-name" class="font-gayathri text-7xl font-bold uppercase mt-20 text-slate-100">X</h1>
-      <h3 id="main-text-role" class="font-gayathri text-5xl font-thin uppercase text-slate-100">X</h3>
+      <h1 id="main-text-name" class="font-gayathri text-2xl md:text-5xl lg:text-6xl xl:text-7xl font-bold uppercase mt-20 text-slate-100">X</h1>
+      <h3 id="main-text-role" class="font-gayathri text-lg md:text-3xl lg:text-4xl xl:text-5xl font-thin uppercase text-slate-100">X</h3>
     </section>
     <!-- ./Main Section -->
 
     <!-- About Section -->
-    <section id="section-about" class="w-full h-screen flex flex-col items-center justify-center">
-      <h1 id="about-text-title" class="font-gayathri text-7xl font-bold text-slate-100">X</h1>
-      <h3 id="about-text-description" class="w-2/3 font-gayathri text-5xl font-thin text-start mt-16 leading-tight text-slate-100">Through 8+ years of my experience, I have been working for over 30+ projects. My tech stacks are Nuxt.js for the front end and Express.js for the back end|</h3>
+    <section id="section-about" class="w-full md:min-h-screen py-8 md:py-0 flex flex-col items-center justify-center">
+      <h1 id="about-text-title" class="font-gayathri text-2xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-slate-100">X</h1>
+      <h3 id="about-text-description" class="w-full md:w-2/3 px-5 md:px-0 font-gayathri text-lg md:text-3xl lg:text-4xl xl:text-5xl font-thin text-start mt-8 md:mt-16 leading-tight text-slate-100">Through 8+ years of my experience, I have been working for over 30+ projects. My tech stacks are Nuxt.js for the front end and Express.js for the back end|</h3>
       
-      <div class="flex flex-row mt-16 font-montaga gap-20">
-        <div id="about-skills-1" class="w-[200px] h-[200px] flex flex-col items-center justify-center ring-black ring-8 rounded-full">
-          <span class="text-5xl">*****</span>
-          <span class="text-3xl">Front End</span>
+      <div class="flex flex-row mt-8 md:mt-16 font-montaga gap-5 md:gap-20">
+        <div id="about-skills-1" class="w-[80px] h-[80px] md:w-[150px] md:h-[150px] lg:w-[200px] lg:h-[200px] flex flex-col items-center justify-center ring-black ring-4 md:ring-8 rounded-full">
+          <span class="text-sm md:text-3xl lg:text-4xl xl:text-5xl">*****</span>
+          <span class="text-sm md:text-2xl lg:text-3xl">Front End</span>
         </div>
 
-        <div id="about-skills-2" class="w-[200px] h-[200px] flex flex-col items-center justify-center ring-black ring-8 rounded-full">
-          <span class="text-5xl">*****</span>
-          <span class="text-3xl">Back End</span>
+        <div id="about-skills-2" class="w-[80px] h-[80px] md:w-[150px] md:h-[150px] lg:w-[200px] lg:h-[200px] flex flex-col items-center justify-center ring-black ring-4 md:ring-8 rounded-full">
+          <span class="text-sm md:text-3xl lg:text-4xl xl:text-5xl">*****</span>
+          <span class="text-sm md:text-2xl lg:text-3xl">Back End</span>
         </div>
 
-        <div id="about-skills-3" class="w-[200px] h-[200px] flex flex-col items-center justify-center ring-black ring-8 rounded-full">
-          <span class="text-5xl">*****</span>
-          <span class="text-3xl">UI Design</span>
+        <div id="about-skills-3" class="w-[80px] h-[80px] md:w-[150px] md:h-[150px] lg:w-[200px] lg:h-[200px] flex flex-col items-center justify-center ring-black ring-4 md:ring-8 rounded-full">
+          <span class="text-sm md:text-3xl lg:text-4xl xl:text-5xl">*****</span>
+          <span class="text-sm md:text-2xl lg:text-3xl">UI Design</span>
         </div>
       </div>
     </section>
@@ -38,13 +38,13 @@
 
     <!-- Projects Section -->
     <section id="section-projects" class="w-full min-h-screen flex flex-col items-center justify-center p-10">
-      <h1 id="projects-text-title" class="font-gayathri text-7xl font-bold text-slate-100">X</h1>
+      <h1 id="projects-text-title" class="font-gayathri text-2xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-slate-100">X</h1>
 
-      <div class="grid grid-cols-3 mt-10 gap-10">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 gap-10">
         <a v-for="(item, index) in projects"
           :key="item"
           :id="`project-item-${index}`"
-          class="card w-96 bg-base-100 shadow-xl group no-underline"
+          class="card bg-base-100 shadow-xl group no-underline"
           :href="item.link"
           target="_blank"
           rel="noreferrer noopener">
@@ -53,7 +53,7 @@
           </figure>
 
           <div class="card-body p-5">
-            <h3 class="text-lg font-medium group-hover:text-primary duration-300">{{ item.title }}</h3>
+            <h3 class="text-sm md:text-lg font-medium group-hover:text-primary duration-300">{{ item.title }}</h3>
           </div>
         </a>
       </div>
@@ -62,16 +62,16 @@
 
     <!-- Contact Section -->
     <section id="section-contact" class="w-full h-screen flex flex-col items-center justify-center relative font-poppins">
-      <div class="w-full flex items-center gap-2 md:gap-10 xl:gap-10">
+      <div class="w-full flex items-center gap-4 md:gap-10 xl:gap-10">
         <div id="contact-image" class="w-5/12 md:w-1/2 xl:w-1/2 flex justify-end">
-          <img id="img-contact" src="https://ik.imagekit.io/b3amk7ihm/1716347873206.webp" class="w-24 min-w-24 md:w-60 md:min-w-60 xl:w-60 xl:min-w-60 h-fit rounded-full drop-shadow-xl"/>
+          <img id="img-contact" src="https://ik.imagekit.io/b3amk7ihm/1716347873206.webp" class="w-20 min-w-20 md:w-60 md:min-w-60 xl:w-60 xl:min-w-60 h-fit rounded-full drop-shadow-xl"/>
         </div>
 
         <div id="contact-divider" class="w-0.5 h-16 md:h-32 xl:h-32 bg-black"></div>
 
         <div class="flex flex-col text-black w-7/12 md:w-1/2 xl:w-1/2 gap-3">
           <div class="flex items-center gap-1 md:gap-2 xl:gap-2">
-            <svg id="contact-icon-instagram" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg id="contact-icon-instagram" class="w-6 h-6 md:w-12 md:h-12" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M24 32C26.1217 32 28.1566 31.1571 29.6569 29.6569C31.1571 28.1566 32 26.1217 32 24C32 21.8783 31.1571 19.8434 29.6569 18.3431C28.1566 16.8429 26.1217 16 24 16C21.8783 16 19.8434 16.8429 18.3431 18.3431C16.8429 19.8434 16 21.8783 16 24C16 26.1217 16.8429 28.1566 18.3431 29.6569C19.8434 31.1571 21.8783 32 24 32Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
               <path d="M6 32V16C6 13.3478 7.05357 10.8043 8.92893 8.92893C10.8043 7.05357 13.3478 6 16 6H32C34.6522 6 37.1957 7.05357 39.0711 8.92893C40.9464 10.8043 42 13.3478 42 16V32C42 34.6522 40.9464 37.1957 39.0711 39.0711C37.1957 40.9464 34.6522 42 32 42H16C13.3478 42 10.8043 40.9464 8.92893 39.0711C7.05357 37.1957 6 34.6522 6 32Z" stroke="black" stroke-width="1.5"/>
               <path d="M35 13.02L35.02 12.998" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -81,7 +81,7 @@
           </div>
 
           <div class="flex items-center gap-1 md:gap-2 xl:gap-2">
-            <svg id="contact-icon-tiktok" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg id="contact-icon-tiktok" class="w-6 h-6 md:w-12 md:h-12" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M33.1998 11.64C31.8327 10.0792 31.0793 8.07486 31.0798 6H24.8998V30.8C24.8522 32.142 24.2856 33.4132 23.3194 34.3459C22.3531 35.2785 21.0627 35.7998 19.7198 35.8C16.8798 35.8 14.5198 33.48 14.5198 30.6C14.5198 27.16 17.8398 24.58 21.2598 25.64V19.32C14.3598 18.4 8.31982 23.76 8.31982 30.6C8.31982 37.26 13.8398 42 19.6998 42C25.9798 42 31.0798 36.9 31.0798 30.6V18.02C33.5858 19.8197 36.5946 20.7853 39.6798 20.78V14.6C39.6798 14.6 35.9198 14.78 33.1998 11.64Z" fill="black"/>
             </svg>
 
